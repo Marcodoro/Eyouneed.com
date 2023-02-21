55c1657a5a0255e779f00b21c0faad4ae43a3cac
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2023-02-21 17:34:20 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/" class="home" :class="{}" :style="{home}">Home</router-link> |
+    <router-link to="/about" class="about" :class="{ hover: hovering }" @mouseover="hover()">About</router-link>
   </nav>
   <router-view/>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      hovering: true,
+      home: {
+        color: `yellow`,
+      }
+    }
+  },
+  computed: {
 
+  },
+  methods: {
+    hover() {
+    }
+  },
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.about {
+  color: #b40202;
 }
 </style>
