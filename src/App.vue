023d98605a6eb6872a94922ef2b1a93286624643
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2023-02-22 17:23:00 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,60 @@
 <template>
-  <nav>
-    <router-link to="/" class="home" :class="{}" :style="{home}">Home</router-link> |
-    <router-link to="/about" class="about" :class="{ hover: hovering }" @mouseover="hover()">About</router-link>
-  </nav>
+  <div class="wrapper">
+    <nav>
+      <font-awesome-icon icon="fa-solid fa-bars" />
+      <router-link to="/" class="home" :class="{hover: hovering}" @mouseleave="notHoveringnow()" @mouseenter="hoveringFunction()">Home</router-link> 
+      <router-link to="/about" class="about" :class="{hover: hovering2}" @mouseleave="notHoveringnow2()" @mouseenter="hoveringFunction2()"> About</router-link>
+    </nav>
+  </div>
   <router-view/>
 </template>
 <script>
 export default {
   data() {
     return {
-      hovering: true,
-      home: {
-        color: `yellow`,
-      }
+      hovering: false,
+      hovering2: false,
     }
   },
-  computed: {
-
-  },
   methods: {
-    hover() {
+    hoveringFunction() {
+      this.hovering = true
+    },
+    notHoveringnow() {
+      this.hovering = false
+    },
+    hoveringFunction2() {
+      this.hovering2 = true
+    },
+    notHoveringnow2() {
+      this.hovering2 = false
     }
   },
 }
 </script>
 <style>
-.about {
-  color: #b40202;
+.hover {
+  font-size: calc(16px + 5px);
+  color: black;
+  box-shadow: rgb(36, 36, 36);
+}
+a {
+  transition: 0.2s ease-in-out font-size;
+  color: rgb(36, 36, 36);
+  text-decoration: none;
+  font-weight: bolder;
+  font-family: Arial, Helvetica, sans-serif;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+nav {
+  display: grid;
+  height: 45px;
+  background-color: aqua;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 45px;
+  text-align: center;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home">
-s
+    <div class="home-content">
+      <div class="getstarted">
+         <div class="btn">
+            <!--Button is here-->
+              <button class="button-31" role="button" @click="openButton()" v-if="opened == false">Button 31</button>
+         </div>
+         <transition name="fade">
+           <div class="parent" :class="{ parent2: opened }" :style="{ gridGap: gap + `px` }">
+              <div @mouseleave="zindex = 1" @mouseover="gapBiggerGrid()" v-if="opened == true" :class="{ usingone: opened }" class="cards one" :style="{ zIndex: zindex }"></div>
+              <div @mouseleave="zindex2 = 1" @mouseover="gapBiggerGrid2()" v-if="opened == true" :class="{ usingtwo: opened }" class="cards two" :style="{ zIndex: zindex2 }"></div>
+              <div @mouseleave="zindex3 = 1" @mouseover="gapBiggerGrid3()" v-if="opened == true" :class="{ usingthree: opened }" class="cards three" :style="{ zIndex: zindex3 }"></div>
+              <div @mouseleave="zindex4 = 1" @mouseover="gapBiggerGrid4()" v-if="opened == true" :class="{ usingfour: opened }" class="cards four" :style="{ zIndex: zindex4 }"></div>
+              <div @mouseleave="zindex5 = 1" @mouseover="gapBiggerGrid5()" v-if="opened == true" :class="{ usingfive: opened }" class="cards five" :style="{ zIndex: zindex5 }"></div>
+              <div @mouseleave="zindex6 = 1" @mouseover="gapBiggerGrid6()" v-if="opened == true" :class="{ usingsix: opened }" class="cards six" :style="{ zIndex: zindex6 }"></div>
+           </div>       
+         </transition>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,133 @@ s
 export default {
   name: 'HomeView',
   components: {
-  }
+  },
+  data() {
+    return {
+      opened: false,
+      gap: 13,
+      zindex: 1,
+      zindex2: 1,
+      zindex3: 1,
+      zindex4: 1,
+      zindex5: 1,
+      zindex6: 1,
+    }
+  },
+  methods: {
+    openButton() {
+      this.opened = true
+    },
+    gapBiggerGrid() {
+      this.zindex = 2
+    },
+    gapBiggerGrid2() {
+      this.zindex2 = 2
+    },
+    gapBiggerGrid3() {
+      this.zindex3 = 2
+    },
+    gapBiggerGrid4() {
+      this.zindex4 = 2
+    },
+    gapBiggerGrid5() {
+      this.zindex5 = 2
+    },
+    gapBiggerGrid6() {
+      this.zindex6 = 2
+    },
+  },
 }
 </script>
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.4s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.home-content {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 200px 0px;
+}
+/* CSS */
+.btn {
+  display: flex;
+  justify-content: center;
+}
+.button-31 {
+  background-color: #222;
+  border-radius: 4px;
+  border-style: none;
+  color: #fff;
+  cursor: pointer;
+  font-family: "Farfetch Basis","Helvetica Neue",Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 1.5;
+  max-width: none;
+  min-height: 44px;
+  min-width: 10px;
+  outline: none;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+  text-transform: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 30vw;
+}
+.cards {
+  position: relative;
+  height: 25vw;
+  width: 29vw;
+  background-color: #222;
+  border-radius: 5px;
+  transition: 0.4s ease-in-out;
+}
+.parent {
+  display: flex;
+  justify-content: center;
+  margin: -100px 0px;
+}
+.usingone {
+  transition: 0.4s ease-in-out;text-align: center;
+}
+.usingtwo {
+  transition: 0.4s ease-in-out;text-align: center;
+}
+.usingthree {
+  transition: 0.4s ease-in-out;text-align: center;
+}
+.usingfour {
+  transition: 0.4s ease-in-out;text-align: center;
+}
+.usingfive {
+  transition: 0.4s ease-in-out;text-align: center;
+}
+.usingsix {
+  transition: 0.4s ease-in-out;text-align: center;
+}
+.parent2 {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  animation-name: usingone;
+  animation-duration: 0.4s;
+  animation-iteration-count: infinite;
+}
+.usingone:hover, .usingfive:hover, .usingfour:hover, .usingtwo:hover, .usingthree:hover, .usingsix:hover {
+  background-color: #363636;
+  transform: scale(1.1);
+}
+@keyframes unsingone {
+  0% {
+    display: none;
+  }
+  100% {
+    display: grid;
+  }
+}
+</style>
